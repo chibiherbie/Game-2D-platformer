47bb6ba03979b283b7fe8fd0ce69139a762f7035
chibiherbie <bekkerrdm@gmail.com>
2020-09-04 08:15:38 +0300

    pull

--- PlatformerUnity/Assets/Scripts/PlayerControl.cs
+++ PlatformerUnity/Assets/Scripts/PlayerControl.cs
@@ -6,17 +6,33 @@ public class PlayerControl : MonoBehaviour
 {
     public GameObject player;
 
-    public rigidbody Rigidbody;
+    public Rigidbody rb;
+    public int speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(transform.position - transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.MovePosition(transform.position + transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(transform.position - transform.right * speed * Time.deltaTime);
+        }
     }
 }
