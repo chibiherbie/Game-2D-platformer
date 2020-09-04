7a4dc84b2207dc390d17a8ed416fecd33ae32e24
Wolfara <Volchonok1414@hotmail.com>
2020-09-04 19:24:47 +0700

    AI

---
+++ PlatformerUnity/Assets/Scripts/AI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    public Transform goal;
+    public NavMeshAgent agent;
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        goal = player.transform;
+        agent.destination = goal.position;
+    }
+}
