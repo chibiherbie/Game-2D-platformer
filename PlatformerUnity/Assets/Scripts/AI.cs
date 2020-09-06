302709fb6d827bdcf681e850a717cf4c530bc70b
chibiherbie <bekkerrdm@gmail.com>
2020-09-06 15:48:19 +0700

    монеты

--- PlatformerUnity/Assets/Scripts/AI.cs
+++ PlatformerUnity/Assets/Scripts/AI.cs
@@ -12,7 +12,7 @@ public class AI : MonoBehaviour
     float attackSped = 1;
     float atakDelay = 1;
     float damage = 10;
-    int randomChislo;
+    int randomNumber;
     public GameObject coin;
     // Start is called before the first frame update
     void Start()
@@ -32,10 +32,12 @@ public class AI : MonoBehaviour
 
         // уничтоение врага
         if (health <= 0) {
-            randomChislo = Random.Range(0, 100);
-            if(randomChislo <= 50)
+
+            randomNumber = Random.Range(0, 10);
+
+            for (int i  = randomNumber; i < 10; i++)
             {
-                Instantiate(coin, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+1, gameObject.transform.position.z), coin.transform.rotation);
+                Instantiate(coin, new Vector3(gameObject.transform.position.x + Random.Range(0.4f, 1.9f), gameObject.transform.position.y+1, gameObject.transform.position.z + Random.Range(0.4f, 1.9f)), coin.transform.rotation);
             }
             Destroy(gameObject);
         }
--- PlatformerUnity/Assets/Scripts/PlayerControl.cs
+++ PlatformerUnity/Assets/Scripts/PlayerControl.cs
@@ -18,7 +18,10 @@ public class PlayerControl : MonoBehaviour
     public Slider hpbar;
     bool is_ground = false; // на земле ли игрок
     public float force = 5;
-    //public float[] weaponList = new float[10];
+    bool checkW;
+    float pickDelay = 0;
+    public int countCoin = 0;
+    public Text coinValue;
 
 
     // Start is called before the first frame update
@@ -28,11 +31,15 @@ public class PlayerControl : MonoBehaviour
 
         health = 100f;
         damage = 5f;
+
+        checkW = false;
     }   
 
     // Update is called once per frame
     void FixedUpdate()
     {   
+        coinValue.text = countCoin.ToString();
+    
         // передвижение персонажа
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -41,26 +48,51 @@ public class PlayerControl : MonoBehaviour
 
         //отображение жизней персонажа
         hpbar.value = health;
+
+        // задержкжа для подбора оружия и атаки
+        if (checkW && pickDelay > 0)
+        {
+            pickDelay -= 1 * Time.deltaTime;
+        }
+
+        else if(checkW && pickDelay <= 0)
+        {
+            checkW = false;
+        }
     }
 
 
     private void OnTriggerStay(Collider other) {
         // атака врага
         if (other.tag == "Enemies") {
-            if (Input.GetKeyDown(KeyCode.F)) {
-                other.GetComponent<AI>().health -= damage;
+            if (Input.GetKey(KeyCode.F)) {
+                if (!checkW ) {
+
+                    checkW = true;
+                    // задержка атаки
+                    pickDelay = 0.5f;
+
+                    other.GetComponent<AI>().health -= damage;
+                }
+                
             }
         }
 
         // подбор оружия
-        if (other.CompareTag("weapon") && Input.GetKeyDown(KeyCode.E)) {
+        if (other.CompareTag("weapon") && Input.GetKey(KeyCode.E)){
             // отоброжение характеристик
+
+
                 
-                // смена оружия
+            if (!checkW) {
+            // смена оружия
                 if (currentWeapon != 0) {
                     weaponNow.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z);
                     weaponNow.transform.parent = null;
                 }
+
+                checkW = true;
+                pickDelay = 0.5f;
 
                 currentWeapon = int.Parse(other.name);
                 for (int i=0; i < 10; i++)
@@ -74,10 +106,13 @@ public class PlayerControl : MonoBehaviour
                     }
                 }
             }
+        }
+
+        
 
         if (other.tag == "ground"){  //если в тригере что то есть и у обьекта тег "ground"
             is_ground = true; //то включаем переменную "на земле"
-            }   
+        }   
     }
     
      void OnTriggerExit(Collider col){              //если из триггера что то вышло и у обьекта тег "ground"
