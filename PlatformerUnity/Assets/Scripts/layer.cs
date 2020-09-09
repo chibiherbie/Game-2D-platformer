05c2d45a5773adfb3f7c5a7f3384d14902c4c700
chibiherbie <bekkerrdm@gmail.com>
2020-09-09 19:38:32 +0700

    персонаж

---
+++ PlatformerUnity/Assets/Scripts/layer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layer : MonoBehaviour
+{   
+
+    GameObject player;
+    public GameObject rock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.z >= rock.transform.position.z - 3){
+            rock.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        }
+        else if (player.transform.position.z < rock.transform.position.z - 3) {
+            rock.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        }
+    }
+}
