1a23b09d6f6eb9e9d691bd6d98ae2334c7558bd8
chibiherbie <bekkerrdm@gmail.com>
2020-09-10 19:56:13 +0700

    первый шаг к мультиплееру

---
+++ PlatformerUnity/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyManager : Photon.PunBehaviour
+{
+    
+    void Awake() 
+    {   
+        print("trying to connect to photon");
+        PhotonNetwork.ConnectUsingSettings("v0.1");
+    }
+
+    public override void OnConnectedToMaster()
+    {   
+        print("Connected to photon");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("Joined room");
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        print("Failed to join room, creating one");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4}, TypedLobby.Default);
+    }
+
+}
